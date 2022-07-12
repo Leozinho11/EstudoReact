1aad37760c85cf1e5bb15032e82898a0ce6a5cf5
Leozinho11 <leoferraribarreto@hotmail.com>
2022-07-12 16:32:31 -0300

    12/07/2022

---
+++ projects/src/App2.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+    state = {
+      name: 'Leozinho 11',
+      counter: 0
+
+  }
+
+
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+        <div className="App">
+         
+         </div>
+     );
+  }
+}
+
+export default App;
